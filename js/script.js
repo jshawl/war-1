6bfd9ba5a503309c96462a6ddf57c6ff3b4e1422
Jesse Shawl <jesse@jshawl.com>
2015-11-04 09:54:01 -0500

    add code comments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 var SUITS = ['&spades;', '&hearts;', '&diams;', '&clubs;'];
 var RANK = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-var DECK = [];
+var DECK = []; // this works! I recommend using lower case variable names
 
 var player1 = document.getElementById('player1');
 var player2 = document.getElementById('player2');
 var start = document.getElementById('start');
 var battle = document.getElementById('battle');
-// var auto = document.getElementById('auto');
+// var auto = document.getElementById('auto'); // remove unused code
 var player1draw = document.getElementById('player1draw');
 
 
-
+// remove empty white space
 
 var buildDeck = function(suits, rank) {
+// you can move lines 1-3 inside this function to prevent "polluting" the global namespace.
   for (var i = 0; i < SUITS.length; i++) {
     for (var j = 0; j < RANK.length; j++) {
       DECK.push({
@@ -59,39 +60,20 @@ var player2StartingHand;
 var player1StartingHandValue;
 var player2StartingHandValue;
 var x;
-var y;
+var y; // what do x and y represent?
+// there are several global variables in your JS. Can you think of a way to declare these variables
+// inside of functions to prevent using globals? Let me know if you'd like to work through this together.
 
 function splitDeck(deck) {
   player1hand.push(deck.slice(0,26));
   player1StartingDeck = player1hand.pop();
-
-  // var firstcard = player1StartingDeck[].value
-  // player1StartingHand = player1StartingDeck.pop();
-  // player1StartingHandValue = player1StartingHand.value;
   player2hand.push(deck.slice(26));
   player2StartingDeck = player2hand.pop();
-
-  // player2StartingHand = play.pop();
-  // player2StartingHandValue = player2StartingHand.value;
-
-
-
 }
 
-
-
-
-
-
-
-
 function compareHand() {
-
-  
-
     var x = player1StartingDeck[0];
     var y = player2StartingDeck[0];
-
 
     if (x.value > y.value) {
         player1.classList.toggle('enable');
@@ -104,12 +86,7 @@ function compareHand() {
 
         player1StartingDeck.push(firstPlayer1);
         player1StartingDeck.push(firstPlayer2);
-
-
-
-
     } else {
-
         player2.classList.toggle('enable');
         player2.innerHTML = 'Player 2 <span id="green">won</span>!' + '<br> Used ' + y.rank + ' of ' + x.suits + '<br> Player 2 has ' + player2StartingDeck.length + ' cards!';
         player1.innerHTML = 'Player 1 <span>lost</span>! <br> Played ' + x.rank +  ' of ' + x.suits + '<br> Player 1 has ' + player1StartingDeck.length + ' cards!';
@@ -123,16 +100,7 @@ function compareHand() {
         player2StartingDeck.push(firstPlayer2);
 
 }
-  //  else {
-   //
-  //       var firstPlayer1 = player1StartingDeck.shift();
-  //       var firstPlayer2 = player2StartingDeck.shift();
-   //
-  //       player1StartingDeck.push(firstPlayer1);
-  //       player1StartingDeck.push(firstPlayer2);
-  //   //
-  //   // }
-
+// indent code correctly to make matching curly braces easier
 }
 
 
@@ -145,16 +113,10 @@ function battleGame() {
   compareHand();
 }
 
-function autoGame () {
-
-}
-
-function populatePlayer1HTML () {
-
-}
-
+// removed unused functions
 
 start.addEventListener('click', startGame);
 battle.addEventListener('click', battleGame);
-// player1draw.addEventListener('click', compareHand )
-// player2.addEventListener('click', )
+
+// overall your JS looks great!! My #1 recommendation is to go through and remove unused code, empty whitespace, 
+// and indent code correctly to increase readability and make debugging easier.
